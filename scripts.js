c9e752f34df3d271effb4b517f587a64682bf844
zachdanz <zachary.danz@gmail.com>
2020-07-22 14:35:55 -0500

    Updating navbar

--- scripts.js
+++ scripts.js
@@ -1,3 +1,28 @@
 AOS.init({
     duration: 1200,
   })
+
+  var fixmeTop = $('.anchor-links').offset().top;
+
+  $(window).scroll(function() {
+      var currentScroll = $(window).scrollTop();
+      if (currentScroll >= fixmeTop){
+          $('.anchor-links').addClass("sticky");
+      } else {
+          $('.anchor-links').removeClass("sticky");
+      }
+  });
+  
+  $('.goSmoothly').click(function(event){		
+      event.preventDefault();
+      $(this).addClass('active');
+      if( $('.anchor-links').hasClass("sticky")) {
+      $('html,body').animate({
+          scrollTop: $(this.hash).offset().top - 100
+      }, 500);
+      } else {
+      $('html,body').animate({
+          scrollTop: $(this.hash).offset().top - 220
+      }, 500);    
+      }
+  });
