e88ee067216a16429594996dd68fba7768207821
zachdanz <zachary.danz@gmail.com>
2020-07-22 18:48:16 -0500

    Added scroll behavior

--- scripts.js
+++ scripts.js
@@ -3,15 +3,20 @@
 $(document).ready(function () {
 	$(window).scroll(function () {
         $('nav').toggleClass("scrolled", ($(window).scrollTop() > 600));
-        console.log("123")
      });
 });
-
-
-
 
 // Collapse Navbar automatically 
 
 $('.navbar-collapse a').click(function(){
     $(".navbar-collapse").collapse('hide');
 });
+
+//Smooth navigation to Nav anchors
+
+// $('.nav-link').click(function() {
+//     var sectionTo = $(this).attr('href');
+//     $('html, body').animate({
+//       scrollTop: $(sectionTo).offset().top
+//     }, 1500);
+// });
