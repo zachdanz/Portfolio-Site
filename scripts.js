3786580c75a74b283060b3049fa9eac8b5c8c90e
zachdanz <zachary.danz@gmail.com>
2020-07-22 17:18:51 -0500

    Added navbar interactivity

--- scripts.js
+++ scripts.js
@@ -1,28 +1,10 @@
-AOS.init({
-    duration: 1200,
-  })
+// Change Nav bar color on scroll
 
-  var fixmeTop = $('.anchor-links').offset().top;
+$(document).ready(function () {
+	$(window).scroll(function () {
+        $('nav').toggleClass("scrolled", ($(window).scrollTop() > 600));
+        console.log("123")
+     });
+});
 
-  $(window).scroll(function() {
-      var currentScroll = $(window).scrollTop();
-      if (currentScroll >= fixmeTop){
-          $('.anchor-links').addClass("sticky");
-      } else {
-          $('.anchor-links').removeClass("sticky");
-      }
-  });
-  
-  $('.goSmoothly').click(function(event){		
-      event.preventDefault();
-      $(this).addClass('active');
-      if( $('.anchor-links').hasClass("sticky")) {
-      $('html,body').animate({
-          scrollTop: $(this.hash).offset().top - 100
-      }, 500);
-      } else {
-      $('html,body').animate({
-          scrollTop: $(this.hash).offset().top - 220
-      }, 500);    
-      }
-  });
+
